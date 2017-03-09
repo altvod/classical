f54611f807612fb1c4e9b1a1a5bacc39c543c48d
Grigory Statsenko <daltvod@gmail.com>
2017-03-09 23:12:25 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='classical',
 
-    version='1.0.0',
+    version='1.0.1',
 
     description='Class Tools for Python',
     long_description=long_description,
